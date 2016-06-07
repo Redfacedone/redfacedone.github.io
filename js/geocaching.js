ede2c904dcc5e058171f8adb97a5d9241d4d204b
Redfacedone <redfacedone@gmail.com>
2016-06-07 09:55:58 -0700

    Create geocaching.js

---
+++ js/geocaching.js
@@ -0,0 +1,8 @@
+check_geocache(){
+var input = document.getElementById("output").value;
+if(input === "I love geocaching!"){
+document.getElementById("geocachingOutput").innerHTML = "You did it!";
+}else{
+document.getElementById("geocachingOutput").innerHTML = "Not quite it."
+};
+};
