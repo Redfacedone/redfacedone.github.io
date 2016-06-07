384f43153b97b21068f7fcc9b71ee46b0c5fa6c0
Redfacedone <redfacedone@gmail.com>
2016-06-07 12:03:52 -0700

    Update geocaching.js

--- js/geocaching.js
+++ js/geocaching.js
@@ -1,6 +1,6 @@
 function check_geocache() {
-var input = document.getElementById("output").value;
-if(input === "I love geocaching!"){
+var geoInput = document.getElementById("output").value;
+if(geoInput === "I love geocaching!"){
 document.getElementById("geocachingOutput").innerHTML = "You did it!";
 }else{
 document.getElementById("geocachingOutput").innerHTML = "Not quite it."
