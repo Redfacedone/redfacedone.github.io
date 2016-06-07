06fde3a9845e3a49573f48718cc5b75b0b598602
Redfacedone <redfacedone@gmail.com>
2016-06-07 12:07:08 -0700

    Update geocaching.js

--- js/geocaching.js
+++ js/geocaching.js
@@ -2,7 +2,9 @@ function check_geocache() {
 var geoInput = document.getElementById("output").value;
 if(geoInput === "I love geocaching!"){
 document.getElementById("geocachingOutput").innerHTML = "You did it!";
-}else{
+}else if(geoInput !== "I love geocaching!"){
 document.getElementById("geocachingOutput").innerHTML = "Not quite it."
-};
+}else{
+document.getElementById("geocachingOutput").innerHTML = "Something's wrong"
+}
 };
